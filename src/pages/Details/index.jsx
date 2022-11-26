66e44cc2a780339ae047205712141a6e415f76dc
Mateus <mateus_raimundo95@email.com>
2022-11-26 21:05:08 -0200

    create page details

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Olá planeta terra! hehe! :) </h1>
+      <span>Meu nome é Mateus, eu sou um psicólogo incrível, trato você super bem, você vai se sentir no céu ao sair de uma consulta comigo, como se nenhum problema existisse.</span>
+    </Container>
+  );
+}
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  max-width: 100%;
+  height: 100vh;
+  background: red;
+  padding: 20px;
+  span {
+    text-decoration: underline;
+    font-size: 24px;
+    font-weight: 800;
+    text-align: justify;
+  }
+`;
